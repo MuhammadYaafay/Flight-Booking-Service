14be003ac7f08be51bf40e030c10fa8c60fad13b
Yaafay <muhammad.yaffay@gmail.com>
2025-04-04 17:56:13 +0500

    Added create booking api

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -1,3 +1,22 @@
-module.exports = {
-    
+const { StatusCodes } = require("http-status-codes");
+const { BookingService } = require("../services");
+const { SuccessResponse, ErrorResponse } = require("../utils/common");
+
+async function createBooking(req, res) {
+  try {
+    const flight = await BookingService.createBooking({
+      flightId: req.body.flightId,
+      userId: req.body.userId,
+      noOfSeats: req.body.noOfSeats
+    });
+    SuccessResponse.data = flight;
+    return res.status(StatusCodes.OK).json(SuccessResponse);
+  } catch (error) {
+    ErrorResponse.error = error;
+    return res.status(error.statusCode).json(ErrorResponse);
+  }
 }
+
+module.exports = {
+  createBooking,
+};
--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -1,3 +1,27 @@
-module.exports = {
-    
+const axios = require("axios");
+
+const { BookingRepository } = require("../repositories");
+const { ServerConfig } = require("../config");
+const db = require("../models");
+const AppError = require("../utils/errors/app-error");
+const { StatusCodes } = require("http-status-codes");
+
+async function createBooking(data) {
+
+  try {
+    const flight = await axios.get(
+      `http://localhost:3000/api/v1/flights/${data.flightId}`
+    );
+    const flightData = flight.data.data;
+    if (data.noOfSeats > flightData.totalSeats) {
+      throw new AppError("no enough seats available", StatusCodes.BAD_REQUEST);
+    }
+    return true;
+  } catch (error) {
+
+  }
 }
+
+module.exports = {
+  createBooking,
+};
