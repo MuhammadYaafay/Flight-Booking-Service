66ec3344afcea441c26eabef3a92521e5a17d2db
Yaafay <muhammad.yaffay@gmail.com>
2025-04-04 18:49:54 +0500

    Fixed booking logic with transactions

--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -6,19 +6,37 @@ const db = require("../models");
 const AppError = require("../utils/errors/app-error");
 const { StatusCodes } = require("http-status-codes");
 
-async function createBooking(data) {
+const bookingRepository = new BookingRepository();
 
+async function createBooking(data) {
+  const transaction = await db.sequelize.transaction();
   try {
     const flight = await axios.get(
       `http://localhost:3000/api/v1/flights/${data.flightId}`
     );
     const flightData = flight.data.data;
+
     if (data.noOfSeats > flightData.totalSeats) {
       throw new AppError("no enough seats available", StatusCodes.BAD_REQUEST);
     }
-    return true;
-  } catch (error) {
 
+    const totalBillingAmount = data.noOfSeats * flightData.price;
+    const bookingPayload = { ...data, totalCost: totalBillingAmount };
+    const booking = await bookingRepository.create(bookingPayload, transaction);
+
+    await axios.patch(
+      `${ServerConfig.FLIGHT_SERVICE}/api/v1/flights/${data.flightId}/seats`,
+      {
+        seats: data.noOfSeats,
+      }
+    ); 
+
+    await transaction.commit();
+    return booking;
+
+  } catch (error) {
+    await transaction.rollback();
+    throw error;
   }
 }
 
