b9cf9390f63a38983df05a63e7f0b738b10fc9ec
Yaafay <muhammad.yaffay@gmail.com>
2025-04-05 16:38:06 +0500

    Added idempotency logic for duplicate payment

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -2,6 +2,8 @@ const { StatusCodes } = require("http-status-codes");
 const { BookingService } = require("../services");
 const { SuccessResponse, ErrorResponse } = require("../utils/common");
 
+const inMemDb = {};
+
 async function createBooking(req, res) {
   try {
     const flight = await BookingService.createBooking({
@@ -19,11 +21,25 @@ async function createBooking(req, res) {
 
 async function makePayment(req, res) {
   try {
+    const idempotencyKey = req.headers["x-idempotency-key"];
+
+    if (!idempotencyKey) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ message: "x-idempotency-key is required" });
+    }
+
+    if (inMemDb[idempotencyKey]) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ message: "Cannot on a successfull transaction" });
+    }
     const flight = await BookingService.makePayment({
       userId: req.body.userId,
       bookingId: req.body.bookingId,
       totalCost: req.body.totalCost,
     });
+    inMemDb[idempotencyKey] = idempotencyKey;
     SuccessResponse.data = flight;
     return res.status(StatusCodes.OK).json(SuccessResponse);
   } catch (error) {
